143bb9edb5b68d08a5811dca9174a31281213b6f
barbtm <128565098+barbtm@users.noreply.github.com>
2023-08-15 14:01:54 -0300

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-function outcome() {
-    let num1 = Number(document.getElementById('num-one').value)
-    let num2 = Number(document.getElementById('num-two').value)
-    let total = 0
-//debugger
-    if (document.getElementById('box1').checked)
-        total = num1 + num2
-    else if (document.getElementById('box2').checked)
-             total = num1 - num2
-    else if (document.getElementById('box3'))
-             total = num1 * num2
-    else 
-     total = num1 / num2
-      document.getElementById('resultArea').innerHTML = 'Result: ' + String(total)
 
-}
